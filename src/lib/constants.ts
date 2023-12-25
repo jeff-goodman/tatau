bda48fb2078242e581456e256faeab0059ec0f96
Jeff Goodman <jeff@goodman.geek.nz>
2023-12-26 08:55:34 +1300

    Refactor out repeated code

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -16,3 +16,12 @@ export const hundred = 'rau';
 export const thousand = 'mano';
 export const million = 'miriona';
 export const billion = 'piriona';
+
+export const numbers: Record<number, string> = {
+    ...ones,
+    10: ten,
+    100: hundred,
+    1_000: thousand,
+    1_000_000: million,
+    1_000_000_000: billion,
+};
